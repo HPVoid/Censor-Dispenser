e483578bb3c99e022eb85ce1bef8d2c6944bf3bf
HPVoid <henry.voigt1@gmx.de>
2020-04-03 12:47:48 +0200

    added censor method for single word or phrase

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -1,11 +1,19 @@
 # These are the emails you will be censoring. The open() function is opening the text file that the emails are contained in and the .read() method is allowing us to save their contexts to the following variables:
 email_one = open("email_one.txt", "r").read()
-
 email_two = open("email_two.txt", "r").read()
-
 email_three = open("email_three.txt", "r").read()
-
 email_four = open("email_four.txt", "r").read()
 
+# print(email_one)
 
-print(email_one, email_two, email_three, email_four)
+def censor(censor_word, text):
+    censor_str = "["
+    for i in range(0, len(censor_word)):
+        censor_str += "-"
+    censor_str += "]"
+
+    return text.replace(censor_word, censor_str)
+
+
+
+print(censor("learning algorithms", email_one))
